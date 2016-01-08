c546f764b9d3806e5350e719409edf1cac723e12
Ken Grimm <ken.grimm@welltok.com>
2016-01-08 14:50:19 -0700

    tabs -> spaces

--- translate_gp.py
+++ translate_gp.py
@@ -8,78 +8,78 @@ import guitarpro
 TEMPO_PERCENT = 0.5
 
 def currentTime():
-  return int(round(time.time() * TEMPO_PERCENT * 1000)) 
+  return int(round(time.time() * TEMPO_PERCENT * 1000))
 
 
 START_TIME = currentTime()
 
 class NoteOnEvent:
-	def __init__(self, track, note, beat):
-		self.name = 'NoteOnEvent'
-		self.track = track
-		self.note = note
-		self.beat = beat
-		self.realStartTime = beat.start + START_TIME 
-		# print self.name + ": " + str(beat.start) + ": " + str(START_TIME) + ": " + str(self.realStartTime)
+  def __init__(self, track, note, beat):
+    self.name = 'NoteOnEvent'
+    self.track = track
+    self.note = note
+    self.beat = beat
+    self.realStartTime = beat.start + START_TIME
+    # print self.name + ": " + str(beat.start) + ": " + str(START_TIME) + ": " + str(self.realStartTime)
 
-	def timeToPlay(self):
-		return currentTime() > self.realStartTime
+  def timeToPlay(self):
+    return currentTime() > self.realStartTime
 
 
 class NoteOffEvent:
-	def __init__(self, track, note, beat):
-		self.name = 'NoteOffEvent'
-		self.track = track
-		self.note = note
-		self.beat = beat
-		self.realStartTime = beat.start + beat.duration.time + START_TIME
+  def __init__(self, track, note, beat):
+    self.name = 'NoteOffEvent'
+    self.track = track
+    self.note = note
+    self.beat = beat
+    self.realStartTime = beat.start + beat.duration.time + START_TIME
 
-		# print self.name + ": " + str(beat.start) + ": " + str(beat.duration.time) + ": " + str(START_TIME) + ": " + str(self.realStartTime)
+    # print self.name + ": " + str(beat.start) + ": " + str(beat.duration.time) + ": " + str(START_TIME) + ": " + str(self.realStartTime)
 
-	def timeToPlay(self):
-		return currentTime() > self.realStartTime
+  def timeToPlay(self):
+    return currentTime() > self.realStartTime
 
 
 def printTabNotes():
-	print "-\n-\n-\n-\n-\n-\n"
+  print "-\n-\n-\n-\n-\n-\n"
 
 def main():
-	filepath = path.join('Rush - Spirit Of The Radio.gp5')
-	song = guitarpro.parse(filepath)
+  filepath = path.join('Rush - Spirit Of The Radio.gp5')
+  song = guitarpro.parse(filepath)
 
-	for track in song.tracks:
-		print "\n\nTrack: " + track.name
-		events = []
+  for track in song.tracks:
+    print "\n\nTrack: " + track.name
+    events = []
 
-		for measure in track.measures:
-			for voice in measure.voices:
-				for beat in voice.beats:
+    for measure in track.measures:
+      for voice in measure.voices:
+        for beat in voice.beats:
 
-					print "Beat start: " + str(beat.start) + ", duration: " + str(beat.duration.time)
+          print "Beat start: " + str(beat.start) + ", duration: " + str(beat.duration.time)
 
-					if beat.effect.chord is not None:
-						print "Chord: " + str(beat.effect.chord.name) + str(beat.effect.chord.strings) + str(beat.effect.chord.fingerings)
+          if beat.effect.chord is not None:
+            print "Chord: " + str(beat.effect.chord.name) + str(beat.effect.chord.strings) + str(beat.effect.chord.fingerings)
 
-					for note in beat.notes:
-						# printTabNotes()
-						# print str(note.type) + ", " + str(note.effect) + ", " + str(note.string) + ": " + str(note.value) + ", real: " + str(note.realValue)
+          for note in beat.notes:
+            # printTabNotes()
+            # print str(note.type) + ", " + str(note.effect) + ", " + str(note.string) + ": " + str(note.value) + ", real: " + str(note.realValue)
 
-						event = NoteOnEvent(track, note, beat)
-						events.append(event)
+            event = NoteOnEvent(track, note, beat)
+            events.append(event)
 
-						event = NoteOffEvent(track, note, beat)
-						events.append(event)
+            event = NoteOffEvent(track, note, beat)
+            events.append(event)
 
 
-			for event in sorted(events, key = eventStartTime) :
-				while(not(event.timeToPlay())):
-					time.sleep(0.001)
+      for event in sorted(events, key = eventStartTime) :
+        while(not(event.timeToPlay())):
+          time.sleep(0.001)
 
-				print event.name + " (" + str(event.realStartTime) + ") : " + str(event.note.string) + ", " + str(event.note.value) + "  (" + str(event.note.realValue) + ")"
+        print event.name + " (" + str(event.realStartTime) + ") : " + str(event.note.string) + ", " + str(event.note.value) + "  (" + str(event.note.realValue) + ")"
 
 
 def eventStartTime(event):
-	return event.realStartTime
+  return event.realStartTime
 
 
 if __name__ == '__main__':
