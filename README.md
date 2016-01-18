85a0da532b16aacade3941c5c4af6952ff69d8f4
Ken <kenmgrimm@gmail.com>
2016-01-18 18:43:57 +0000

    Some notes, README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # guitar_pro
+
+Must install pyGuitarPro:
+cd PyGuitarPro-0.2.2/
+sudo python setup.py install
+
+
+javac MidiSynth.java
+
+# Note:
+  If screen size is too small, curses will error out on a call to addStr.  Make text smaller to fit more on the screen
+
+# Run with 
+
+./run.sh
+
+
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-javac MidiSynth.java && python translate_gp.py 2> test.txt ; reset ; killall java
+echo 'Set audio with(where n is 0=auto, 1=headphones, 2=hdmi):  sudo amixer -c 0 cset numid=3 1'
+
+javac MidiSynth.java && python translate_gp.py 2> translate_gp.err ; reset ; killall java
+
